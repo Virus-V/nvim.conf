e2ddb5d42945b306821ad7a0dfd97929094c9a3d
virusv <virusv@MiWiFi-RA67-srv.xiaomi>
2025-02-06 10:21:57 +0800

    update treesitter config

--- init.lua
+++ init.lua
@@ -111,35 +111,17 @@ plugins = {
   -- 语法高亮插件
   {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-      require("nvim-treesitter.configs").setup {
-        -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-        ensure_installed = { "c", "cpp", "make", "vim", "go" },
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
 
-        -- Install languages synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- List of parsers to ignore installing
-        ignore_install = { "javascript" },
-
-        highlight = {
-          -- `false` will disable the whole extension
-          enable = true,
-
-          -- list of language that will be disabled
-          -- disable = { "c", "rust" },
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-      }
-    end,
-    config = function()
-      require("nvim-treesitter.install").update({ with_sync = true })
-    end,
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "markdown" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+    end
   },
 
   -- LSP(Language Server Protocol)
